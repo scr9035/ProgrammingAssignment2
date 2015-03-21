4be675e8974fb2684f43c2a125b87bb38b166798
scr9035 <scr9035@gmail.com>
2015-03-21 16:14:03 -0400

    cleaned up some comments for easy reading.

--- cachematrix.R
+++ cachematrix.R
@@ -1,4 +1,12 @@
 makeCacheMatrix <- function(x = matrix()) {
+                                                    #Essentially creates a list of four functions. Checks to see
+                                                    #if the matrix is in cache, if so move on, if not set it to cache.
+                                                    #Also checks to see if the inverse of a matrix asked to be inverted
+                                                    #is in the cache. If so, pass that, if not invert the matrix the set it to cache.
+                                                    #The control of these functions is more clearly seen in the function
+                                                    #that calls the list. The cacheSolve function below.
+        
+        
         
         invMatrix <- NULL       #Intitalized the inverse matrix variable 
                                 #to NUll so the makeCacheMatrix function 
@@ -20,7 +28,7 @@ makeCacheMatrix <- function(x = matrix()) {
         getMatrixInv <- function() invMatrix  #Create function 'getMatrixInv'
                                               #in  makeCacheMatrix that with matrix variable 'invMatrix'
                                               #(basically returns invMatrix variable,
-                                              #but returns has a specific meaning)
+                                              #but return has a specific meaning so I don't want to use that word.)
         
         setMatrixInv <- function(solveM) invMatrix <<- solveM   #Create function 'setMatrixInv' in
                                                                 #makeCacheMatrix that with matrix 
@@ -36,7 +44,10 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 cacheSolve <- function(x, ...) {
-                                                 #Return a matrix that is the inverse of 'x'. 
+                                                 #Return a matrix that is the inverse of 'x'. If the matrix inversion
+                                                 #in the cache it doesn't solve(matrix) it just grabs the solution from cache.
+                                                 #if not it solves the matrix, then stores it in cache.
+        
                                                  #the list from makeCacheMatrix function is passed into it.
         solveM <- x$getMatInv()                  #makes variable solveM the function 'getMatInv()' from MakeCacheMatrix list 
         
